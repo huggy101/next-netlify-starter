27271ae06c3d90e5b4ab0a2eef746cb3b2714557
huggy101 <44769893+huggy101@users.noreply.github.com>
2024-01-30 14:19:40 +0000

    put logo on

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to Mir's Website!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <img src="public\IMG-20240124-WA004.jpg">
         </p>
       </main>
 
