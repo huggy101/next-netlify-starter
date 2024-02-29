f044edd144e09d2178fce270a581517f1ad3ff90
huggy101 <44769893+huggy101@users.noreply.github.com>
2024-02-29 15:06:21 +0000

    Update index.js
    striiped down

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,11 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
 
-      <main>
     		<script>
 			window.location.replace("https://www.google.com/");
 		</script>
-        <Header title="Welcome to Mir's Website!" />
-       
-          <img src="IMG-20240124-WA0004.jpg" />
-        
-      </main>
-
-      <Footer />
-    </div>
+    </>
   )
 }
