3c3aa917fe08b6fc026f1075e03986040837125d
huggy101 <44769893+huggy101@users.noreply.github.com>
2024-02-29 15:10:47 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
 
 export default function Home() {
   return (
-    <>
-
-    		<script>
-			window.location.replace("https://www.google.com/");
-		</script>
-    </>
+ <Redirect to="https://www.google.com/" />;
   )
 }
